a2b69d8b08a10c6a1671314cb92f2f689fa367d1
q695933585 <695933585@qq.com>
2018-07-31 08:38:44 +0000

    tang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Laravel! - by kebi
+                    唐昕阳
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a >Outstanding youth</a>
+                    <a >Chairman of the blower factory</a>
+                    <a >NPC deputy</a>
+                    <a >....</a>
                 </div>
             </div>
         </div>
